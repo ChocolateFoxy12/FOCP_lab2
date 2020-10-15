0789d7663c40f674e86a41d4dddf4707d848b2b2
Katarzyna <kataszl768@student.polsl.pl>
2020-10-15 12:14:43 +0200

    2

--- lab2/1.cpp
+++ lab2/1.cpp
@@ -2,3 +2,22 @@
 
 using namespace std;
 main ()
+{
+    //read a number from console
+    //figure out if the number is prime or not
+    //print the result to the console
+
+    int number;
+
+    cout<<"please introduce the number: ";
+    cin>>number; cout<<endl;
+
+    for (int i=2;i<number;i++)
+    {
+        if (number%i==0){cout<<"the number's not prime ;/"<<endl;}
+        return 0;
+    }
+    cout<<"the number's  prime :>"<<endl;
+
+    return 0;
+}
